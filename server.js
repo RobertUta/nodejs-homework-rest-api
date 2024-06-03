e83e01bc97e4c3397936158d05c655e943a2d5f4
Uță Robert Constantin <roy_robert_99@yahoo.com>
2024-06-03 14:53:43 +0300

    hw
    hw

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const { app } = require("./app");
+const { app } = require("./src/app");
 
 const PORT = 3000;
 mongoose.set("strictQuery", true);
 
 const connection = mongoose.connect(process.env.CONNECTION_STRING);
 connection
-  .then(() => {
-    app.listen(PORT, function () {
-      console.log("Database connection successful");
-      console.log(`Server running. Use our API on port: ${PORT}`);
+    .then(() => {
+        app.listen(PORT, function () {
+            console.log("Database connection successful");
+            console.log(`Server running. Use our API on port: ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(`Server not running. Error message: ${err.message}`);
+        process.exit(1);
     });
-  })
-  .catch((err) => {
-    console.log(`Server not running. Error message: ${err.message}`);
-    process.exit(1);
-  });
